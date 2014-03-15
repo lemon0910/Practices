a0c4050e738ed0059c13758bb0ed9cd03639b700
lemon <lemon_wonder@163.com>
2014-03-16 00:16:00 +0800

    quicksort

---
+++ sort/QuickSort.cpp
@@ -0,0 +1,22 @@
+void QuickSort(int s[], int l, int r)
+{
+	if (l < r)
+	{
+		int i = l, j = r, x = s[l];
+		while (i < j)
+		{
+			while (i < j && s[j] >= x)
+				j--;
+			if (i < j)
+				s[i++] = s[j];
+			
+			while (i < j && s[i] < x)
+				i++;
+			if (i < j)
+				s[j--] = s[i];
+		}
+		s[i] = x;
+		QuickSort(s, l, i - 1);
+		QuickSort(s, i + 1, r);
+	}
+}
