a8186ef1715f376a214259f08ef5142c5b020394
lemon <lemon_wonder@163.com>
2014-08-13 09:20:57 +0800

    auto make

---
+++ make/reposit/suites.h
@@ -0,0 +1,38 @@
+/**
+ * @file suites.h
+ * @comment
+ * define all test suites here
+ *
+ * @author niexw
+ * @email xiaowen.nie.cn@gmail.com
+ */
+ 
+#ifndef _TEST_SUITES_H_
+#define _TEST_SUITES_H_
+
+#include "LogTest.h"
+#include "ExceptionTest.h"
+
+class LogTestSuite : public test_suite
+{
+  public:
+	LogTestSuite() : test_suite("LogTestSuite")
+	{
+		boost::shared_ptr<LogTestCases> instance(new LogTestCases());
+		add(BOOST_CLASS_TEST_CASE(&LogTestCases::testLog, instance));
+	}
+};
+
+class ExceptionTestSuite : public test_suite
+{
+  public:
+	ExceptionTestSuite() : test_suite("ExceptionTestSuite")
+	{
+		boost::shared_ptr<ExceptionTestCases> instance(new ExceptionTestCases());
+		add(BOOST_CLASS_TEST_CASE(&ExceptionTestCases::testException, instance));
+	}
+};
+
+
+#endif // _TEST_SUITES_H_
+
