8af235c8795c8ebedfba12e239a1dfd44a941564
lemon0910 <lemon_wonder@163.com>
2014-12-12 17:10:58 +0800

    mybuddy

---
+++ buddy/mybuddy.h
@@ -0,0 +1,17 @@
+#ifndef __MYBUDDY__
+#define __MYBUDDY__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct mybuddy;
+struct mybuddy* mybuddy_new(int size);
+void mybuddy_destroy(struct mybuddy *self);
+
+int mybuddy_alloc(struct mybuddy* self, int size);
+void mybuddy_free(struct mybuddy*i self, int offset);
+
+int mybuddy_size(struct mybuddy *selft, int offset);
+void mybuddy_dump(struct mybuddy* self);
+
+#endif
